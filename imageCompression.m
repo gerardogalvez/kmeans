13a5b2610f674fee18c9c5fbc0cc0383bec96d70
Gerardo Galvez <gerardogv96@hotmail.com>
2016-12-07 19:54:28 -0600

    Save compressed images

--- imageCompression.m
+++ imageCompression.m
@@ -1,30 +1,46 @@
+% K-means clustering algorithm for image compression.
 clear ; close all; clc;
+
+% Starting K
 K = 2;
+
 MAX_ITERS = 10;
 
 img = double(imread('birds.jpg'));
-img = img / 255;
+img = img / 255; % Scale image to values between 0 and 1
 imgSize = size(img);
 
 figure('Position', [0 0 1240 680]);
 subplot(2, 3, 1);
-imshow(img);
+imshow(img); % Plot original image
 title('Original');
 
 subplotIndex = 2;
 
+% Run k-means for K = 2, 4, 8, 16, and 32
 while (K <= 32)
+	% Reshape m x n image to a m*n x 3 matrix
 	X = reshape(img, imgSize(1) * imgSize(2), 3);
+
+	% Randomly choose K points as centroids
 	centroids = initializeCentroids(X, K);
 
 	fprintf('Running K-means with K = %d\n', K);
-	[centroids C] = kmeans(X, centroids, MAX_ITERS);
-	C = findClosestCentroids(X, centroids);
 
+	% Get centroids and to which centroid each point is assigned
+	[centroids C] = kmeans(X, centroids, MAX_ITERS);
+
+	% Set each point to the centroid it is assigned
 	X = centroids(C, :);
 
+	% Reshape m*n x 3 matrix to an mxn image
 	compressedImage = reshape(X, imgSize(1), imgSize(2), 3);
 	
+	% Save image
+	filename = strcat('birds', num2str(K), '.jpg');
+	imwrite(compressedImage, filename);
+	
+	% Plot compressed image
 	subplot(2, 3, subplotIndex);
 	imshow(compressedImage);
 	title(sprintf('Compressed (K = %d)', K));
@@ -32,4 +48,3 @@ while (K <= 32)
 	K = K * 2;
 	subplotIndex = subplotIndex + 1;
 endwhile
-
--- kmeans.m
+++ kmeans.m
@@ -3,13 +3,31 @@ function [centroids C] = kmeans(X, centroids, MAX_ITER)
 	m = size(X, 1); % Number of examples
 	K = size(centroids, 1); % Number of clusters
 	C = zeros(m, 1); % Indexes to which cluster each example is assigned
+	iter = 0;
 
-	for iter = 1 : MAX_ITER
-		fprintf('Iteration %d/%d\n', iter, MAX_ITER);
+	oldCentroids = rand(size(centroids));
+	while (iter < MAX_ITER && oldCentroids != centroids)
+
+		oldCentroids = centroids;
+
+		fprintf('Iteration %d/%d\n', iter+1, MAX_ITER);
 		if exist('OCTAVE_VERSION')
         	fflush(stdout);
+    	
     	endif
 		C = findClosestCentroids(X, centroids);
 		centroids = updateCentroids(X, C, K);
-	endfor
+		
+		iter = iter + 1;
+	endwhile
+
+	if (iter == MAX_ITER)
+		printf('Reached last iteration.\n\n');
+    else
+    	printf('Reached convergence\n\n')
+    endif
+    
+    if exist('OCTAVE_VERSION')
+        	fflush(stdout);
+	endif
 end
